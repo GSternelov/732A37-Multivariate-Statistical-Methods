dc6118fe834059a8c46621df2407b244ebc8f7df
sternis <gustav.sternelov@hotmail.com>
2015-12-02 09:09:31 +0100

    Lab 3, working...

--- Lab3/Lab3.R
+++ Lab3/Lab3.R
@@ -23,6 +23,7 @@ lambda=prComp$sdev^2
 sprintf("%2.3f",lambda/sum(lambda)*100)[1:2]
 
 ### c)
+prComp$rotation[,1:2]
 # data frame with scores and country
 pcScores <- data.frame(PC1=prComp$x[,1], PC2=prComp$x[,2], country=data[,1])
 library(ggplot2)
@@ -38,10 +39,62 @@ corFA <- cor(data[, 2:8])
 covFA <- cov(data[, 2:8])
 
 # With covariance matric
-FAcov <- factanal(data=(data[,2:8]),covmat =covFA, factors = 3, scores = "regression")
+FAcov <- factanal(data=(data[,2:8]),covmat =covFA, factors = 3, scores = "Bartlett")
 
 # With correlation matrix
-FAcor <- factanal(x=data[,2:8], factors = 3, scores = "regression")
-FAcor$scores
+FAcor <- factanal(x=data[,2:8], factors = 2, scores = "Bartlett")
+FAcor$
+plot(FAcor$scores)
 
 
+
+## Principal component solution with S
+covEigenV <- as.matrix(data.frame(eigen(cov(data[, 2:8]))[1]))
+covEigenVec <- as.matrix(data.frame(eigen(cov(data[, 2:8]))[2]))
+
+L <- matrix(ncol=7, nrow=7) 
+for(i in 1:7){
+  L[,i] <- sqrt(covEigenV[i,]) * covEigenVec[,i]
+}
+TestPsi <- diag(x=diag(covFA - L%*%t(L)))
+#Sii <- diag(covFA)
+#Psi <- Sii -  colSums(L%*%t(L))
+#PsiHat <- diag(x=Psi)
+
+diag(L%*%t(L) + TestPsi)
+diag(covFA)
+residualMat <- covFA - (L%*%t(L) + TestPsi)
+# Proportion of total sample variance explained
+covEigenV[1,] / sum(diag(covFA))
+covEigenV[2,] / sum(diag(covFA))
+
+factorLoadCov <- L[,1]
+# Factor scores
+dataCtrd <- as.matrix(scale(data[,2:8], center=TRUE, scale=FALSE))
+ScoresCov <- t(solve(t(L)%*%L) %*% t(L) %*% t(dataCtrd))
+plot(ScoresCov[,1], ScoresCov[,2])
+
+## Principal component solution with R
+corEigenV <- as.matrix(data.frame(eigen(cor(data[, 2:8]))[1]))
+corEigenVec <- as.matrix(data.frame(eigen(cor(data[, 2:8]))[2]))
+
+Lcor <- matrix(ncol=7, nrow=7) 
+for(i in 1:7){
+  Lcor[,i] <- sqrt(corEigenV[i,]) * corEigenVec[,i]
+}
+TestPsi <- diag(x=diag(corFA - Lcor%*%t(Lcor)))
+
+residualMat <- corFA - (Lcor%*%t(Lcor) + TestPsi)
+# Proportion of total sample variance explained
+corEigenV[1,] / 7
+corEigenV[2,] / 7
+
+factorLoadCor <- Lcor[,1:2]
+# Factor scores
+dataCtrd <- as.matrix(scale(data[,2:8], center=TRUE, scale=FALSE))
+ScoresCor <- t(solve(t(Lcor)%*%Lcor) %*% t(Lcor) %*% t(dataCtrd))
+
+plot(ScoresCor[,1], ScoresCor[,2])
+
+# The ML method, common factors F and specific factors epsilon
+# assumed to be normally dsitributed
